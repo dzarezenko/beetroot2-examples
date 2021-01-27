a3f033ab4e40515abc9223ed96bc0f5a1ac348a8
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-01-27 20:46:08 +0200

    Input component created

---
+++ src/components/Input.vue
@@ -0,0 +1,19 @@
+<template>
+  <input @keyup="showMessage($event)" />
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World!!!",
+    }
+  },
+  methods: {
+    showMessage(event) {
+      this.message = event.target.value;
+    }
+  },
+}
+</script>
