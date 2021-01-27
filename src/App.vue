870534244da874fe6f031f36eafb07234f64ed33
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-01-27 19:40:26 +0200

    Simple event example

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <input @keyup="showMessage($event)" />
+  <h1>{{ message }}</h1>
+  <p>{{ message }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World!!!",
+    }
+  },
+  methods: {
+    showMessage(event) {
+      this.message = event.target.value;
+    }
+  }
+}
+</script>
